65fd0bc9fa86cf0d8057e9ec5d9cc5e8cf460b6d
toyess-37 <padhiharytaes@gmail.com>
2026-08-04 10:37:38 +0530

    added autograd basics from code-along with andrej karpathy's video

---
+++ autograd/nn.py
@@ -0,0 +1,57 @@
+from value import Value, draw_dot
+import random
+
+class Module:
+  def zero_grad(self):
+    for p in self.parameters():
+      p.grad = 0.0
+
+  def parameters(self):
+    return []
+
+class Neuron(Module):
+  def __init__(self, nin):
+    self.w = [Value(random.uniform(-1,1)) for _ in range(nin)]
+    self.b = Value(random.uniform(-1,1))
+
+  def __call__(self, x):
+    # w*x + b
+    act = sum((wi * xi for wi, xi in zip(self.w, x)), self.b)
+    out = act.relu()
+    return out
+
+  def parameters(self):
+    return self.w + [self.b]
+
+class Layer(Module):
+  def __init__(self, nin, nout):
+    self.neurons = [Neuron(nin) for _ in range(nout)]
+
+  def __call__(self, x):
+    outs = [n(x) for n in self.neurons]
+    return outs[0] if len(outs) == 1 else outs
+
+  def parameters(self):
+    return [p for neuron in self.neurons for p in neuron.parameters()]
+
+class MLP(Module):
+  def __init__(self, nin, nouts):
+    sz = [nin] + nouts
+    self.layers = [Layer(sz[i], sz[i+1]) for i in range(len(nouts))]
+
+  def __call__(self, x):
+    for layer in self.layers:
+      x = layer(x)
+    return x
+
+  def parameters(self):
+    return [p for layer in self.layers for p in layer.parameters()]
+
+x = [2.0, 3.0, -1.0]
+n = MLP(3, [4, 4, 1])
+
+out = n(x)
+print("Forward pass output:", out)
+
+dot = draw_dot(out)
+dot.render('mlp_graph_relu', view=True)
---
+++ autograd/value.py
@@ -0,0 +1,150 @@
+from graphviz import Digraph
+import os
+import math
+os.environ["PATH"] += os.pathsep + r'C:\Program Files\Graphviz\bin'
+
+class Value:
+  def __init__(self, data, _children=(), _op='', label=''):
+    self.data = data
+    self.grad = 0.0
+    self._backward = lambda: None
+    self._prev = set(_children)
+    self._op = _op
+    self.label = label
+
+  def __repr__(self):
+    return f"Value(data={self.data})"
+
+  def __add__(self, other):
+    other = other if isinstance(other, Value) else Value(other)
+    out = Value(self.data + other.data, (self, other), '+')
+
+    def _backward():
+      self.grad += 1 * out.grad
+      other.grad += 1 * out.grad
+
+    out._backward = _backward
+    return out
+
+  def __mul__(self, other):
+    other = other if isinstance(other, Value) else Value(other)
+    out = Value(self.data * other.data, (self, other), '*')
+
+    def _backward():
+      self.grad += other.data * out.grad
+      other.grad += self.data * out.grad
+    
+    out._backward = _backward
+    return out
+
+  def tanh(self):
+    x = self.data
+    t = (math.exp(2*x)-1)/(math.exp(2*x)+1)
+    out = Value(t, (self,), 'tanh')
+
+    def _backward():
+      self.grad += (1 - t**2) * out.grad
+
+    out._backward = _backward
+    return out
+
+  def relu(self):
+    out = Value(max(0, self.data), (self,), 'relu')
+
+    def _backward():
+      self.grad += (out.data > 0) * out.grad
+
+    out._backward = _backward
+    return out
+
+  def backward(self):
+    self.grad = 1.0
+
+    topo = []
+    visited = set()
+
+    def build_topo(v):
+      if v not in visited:
+        visited.add(v)
+        for child in v._prev:
+          build_topo(child)
+        topo.append(v)
+
+    build_topo(self)
+
+    for node in reversed(topo):
+      node._backward()
+
+  def zero_grad(self):
+    topo = []
+    visited = set()
+
+    def build_topo(v):
+      if v not in visited:
+        visited.add(v)
+        for child in v._prev:
+          build_topo(child)
+        topo.append(v)
+
+    build_topo(self)
+
+    for node in topo:
+      node.grad = 0.0
+
+def trace(root):
+  # builds set of all nodes and edges in the graph
+  nodes, edges = set(), set()
+  def build(v):
+    if v not in nodes:
+      nodes.add(v)
+      for child in v._prev:
+        edges.add((child, v))
+        build(child)
+  build(root)
+  return nodes, edges
+
+def draw_dot(root):
+  dot = Digraph(format='pdf', graph_attr={'rankdir': 'LR'},  
+                node_attr={'fontname': 'Inconsolata'}, 
+                edge_attr={'fontname': 'Inconsolata'})
+
+  nodes, edges = trace(root)
+  for n in nodes:
+    uid = str(id(n))
+    dot.node(name = uid, label="{ %s | data %.4f | grad %.4f }" % (n.label, n.data, n.grad), shape='record')
+    if n._op:
+      op_uid = uid + "_op_" + n._op
+      dot.node(name = op_uid, label = n._op)
+      dot.edge(op_uid, uid)
+
+  for n1, n2 in edges:
+    op_uid = str(id(n2)) + "_op_" + n2._op
+    dot.edge(str(id(n1)), op_uid)
+
+  return dot
+
+def check_gradient(f, var, eps=1e-6):
+  var.data += eps
+  out_plus = f().data
+
+  var.data -= 2*eps
+  out_minus = f().data
+
+  var.data += eps
+
+  numerical_grad = (out_plus - out_minus) / (2*eps)
+  return numerical_grad
+
+
+a = Value(2.0, label='a')
+b = Value(-3.0, label='b')
+c = Value(10.0, label='c')
+
+def lol():
+  return a*a+a
+
+L = lol()
+L.backward()
+L.zero_grad()
+L.backward()
+print(f"a.grad = {a.grad}")
